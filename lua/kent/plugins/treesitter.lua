d20b6e27883940252a7f280d91185fd10926d5d2
user <user@userdeMacBook-Pro-2.local>
2024-05-14 14:15:29 +0800

    setup nvim-tree

--- lua/kent/plugins/treesitter.lua
+++ lua/kent/plugins/treesitter.lua
@@ -1,35 +1,46 @@
+-- 參考: https://www.josean.com/posts/nvim-treesitter-and-textobjects
 return {
 	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
 	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
 			ensure_installed = {
-				"c",
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
 				"lua",
 				"vim",
-				"vimdoc",
+				"dockerfile",
+				"gitignore",
 				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"html",
-				"typescript",
-				"rust",
-				"vue",
-				"css",
-				"json",
-				"scss",
-				"yaml",
-				"markdown",
-				"sql",
 			},
-			sync_install = false,
-			highlight = { enable = false },
-			indent = { enable = true },
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>", -- 按下 Ctrl + space 进入增量选择模式, 會進入visual模式, 並且選擇當前的節點
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
 		})
 	end,
 }
