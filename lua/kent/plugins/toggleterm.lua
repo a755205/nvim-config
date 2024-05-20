81124ec962746b08414bdbf704c9ee296bda438a
user <user@userdeMacBook-Pro-2.local>
2024-05-20 14:37:13 +0800

    lazygit desc

--- lua/kent/plugins/toggleterm.lua
+++ lua/kent/plugins/toggleterm.lua
@@ -19,6 +19,11 @@ return {
 			lazygit:toggle()
 		end
 
-		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>g",
+			"<cmd>lua _lazygit_toggle()<CR>",
+			{ noremap = true, silent = true, desc = "Toggle Lazygit" }
+		)
 	end,
 }
