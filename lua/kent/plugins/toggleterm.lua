abb0bbe0346928bcde3924a4f558e70c3be75068
Kent <kent.cheng@pgtalk.com>
2024-09-21 12:56:46 +0800

    change tablemode toggle keymap and desc

--- lua/kent/plugins/toggleterm.lua
+++ lua/kent/plugins/toggleterm.lua
@@ -4,16 +4,22 @@ return {
 	config = function()
 		local toggleterm = require("toggleterm")
 		toggleterm.setup({
-			size = 20,
-			-- open_mapping = [[<leader>t]],
-			direction = "tab", -- direction = 'vertical' | 'horizontal' | 'tab' | 'float' ,
+			size = 80,
+			-- open_mapping = [[<C-\>]], -- use this in office, macbook woth bluetooth keybord
+			open_mapping = [[<D-l>]], -- work in my macbook, with command
+			direction = "vertical", -- direction = 'vertical' | 'horizontal' | 'tab' | 'float' ,
 			float_opts = {
 				border = "single", -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open ]]
 			},
 		})
 
 		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "tab",
+			display_name = "L.A.Z.Y.G.I.T",
+		})
 
 		function _lazygit_toggle()
 			lazygit:toggle()
@@ -21,9 +27,9 @@ return {
 
 		vim.api.nvim_set_keymap(
 			"n",
-			"<leader>gg",
+			"<leader>tg",
 			"<cmd>lua _lazygit_toggle()<CR>",
-			{ noremap = true, silent = true, desc = "[Lazygit]" }
+			{ noremap = true, silent = true, desc = "Lazygit" }
 		)
 	end,
 }
--- lua/kent/plugins/vim-table-mode.lua
+++ lua/kent/plugins/vim-table-mode.lua
@@ -1,3 +1,10 @@
 return {
 	"dhruvasagar/vim-table-mode",
+	config = function()
+		vim.api.nvim_set_keymap("n", "<leader>tm", "<cmd>TableModeToggle<CR>", {
+			noremap = true,
+			silent = true,
+			desc = "TableMode Toggle",
+		})
+	end,
 }
