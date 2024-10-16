c497da523c575ac31174e3501f5a29ebbb38e02d
user <user@userdeMacBook-Pro-2.local>
2024-10-16 16:48:53 +0800

    setup new lualine config

--- lua/kent/plugins/lualine.lua
+++ lua/kent/plugins/lualine.lua
@@ -21,26 +21,22 @@ return {
 			yellow = "#ECBE7B",
 			cyan = "#008080",
 			darkblue = "#081633",
-			green = "#98be65",
-			orange = "#FF8800",
+			-- green = "#98be65",
+			-- orange = "#FF8800",
 			violet = "#a9a1e1",
 			magenta = "#c678dd",
 			blue = "#51afef",
-			red = "#ec5f67",
-			-- [dracula] Color Palette
-			-- Palette	Hex	RGB	HSL	Color Picker Boxes
-			-- Background	#282a36	40 42 54	231° 15% 18%	Background Color
-			-- Current Line	#44475a	68 71 90	232° 14% 31%	Current Line Color
-			-- Selection	#44475a	68 71 90	232° 14% 31%	Selection Color
-			-- Foreground	#f8f8f2	248 248 242	60° 30% 96%	Foreground Color
-			-- Comment	#6272a4	98 114 164	225° 27% 51%	Comment Color
-			-- Cyan	#8be9fd	139 233 253	191° 97% 77%	Cyan Color
-			-- Green	#50fa7b	80 250 123	135° 94% 65%	Green Color
-			-- Orange	#ffb86c	255 184 108	31° 100% 71%	Orange Color
-			-- Pink	#ff79c6	255 121 198	326° 100% 74%	Pink Color
-			-- Purple	#bd93f9	189 147 249	265° 89% 78%	Purple Color
-			-- Red	#ff5555	255 85 85	0° 100% 67%	Red Color
-			-- Yellow	#f1fa8c	241 250 140	65° 92% 76%	Yellow Color
+			-- red = "#ec5f67",
+			red = "#ca1243",
+			grey = "#a0a1a7",
+			black = "#383a42",
+			white = "#f3f3f3",
+			light_green = "#83a598",
+			orange = "#fe8019",
+			green = "#8ec07c",
+			purple = "#bd93f9",
+			dark_purple = "#5d4d7a",
+
 			dracula = {
 				bg = "#282a36",
 				fg = "#f8f8f2",
@@ -61,6 +57,56 @@ return {
 			},
 		}
 
+		local theme = {
+			normal = {
+				a = { fg = colors.white, bg = colors.dark_purple },
+				b = { fg = colors.white, bg = colors.black },
+				c = { fg = colors.white, bg = colors.black },
+				x = { fg = colors.white, bg = colors.black },
+				y = { fg = colors.white, bg = colors.black },
+				z = { fg = colors.white, bg = colors.black },
+			},
+			insert = { a = { fg = colors.black, bg = colors.light_green } },
+			visual = { a = { fg = colors.black, bg = colors.orange } },
+			replace = { a = { fg = colors.black, bg = colors.green } },
+		}
+
+		local empty = require("lualine.component"):extend()
+		function empty:draw(default_highlight)
+			self.status = ""
+			self.applied_separator = ""
+			self:apply_highlights(default_highlight)
+			self:apply_section_separators()
+			return self.status
+		end
+
+		-- Put proper separators and gaps between components in sections
+		local function process_sections(sections)
+			for name, section in pairs(sections) do
+				local left = name:sub(9, 10) < "x"
+				for pos = 1, name ~= "lualine_z" and #section or #section - 1 do
+					table.insert(section, pos * 2, { empty, color = { fg = colors.white, bg = colors.white } })
+				end
+				for id, comp in ipairs(section) do
+					if type(comp) ~= "table" then
+						comp = { comp }
+						section[id] = comp
+					end
+					comp.separator = left and { right = "" } or { left = "" }
+				end
+			end
+			return sections
+		end
+
+		local function modified()
+			if vim.bo.modified then
+				return "+"
+			elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+				return "-"
+			end
+			return ""
+		end
+
 		require("battery").setup({
 			update_rate_seconds = 30, -- Number of seconds between checking battery status
 			show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
@@ -70,86 +116,90 @@ return {
 			vertical_icons = false, -- When true icons are vertical, otherwise shows horizontal battery icon
 			multiple_battery_selection = 1, -- Which battery to choose when multiple found. "max" or "maximum", "min" or "minimum" or a number to pick the nth battery found (currently linux acpi only)
 		})
-		-- plubin [battery]
-		local battery = {
-			function()
-				return require("battery").get_status_line()
-			end,
-			color = {
-				fg = colors.violet,
-				-- bg = colors.bg
-			},
+
+		local diagnostics = {
+			"diagnostics",
 		}
+
 		lualine.setup({
 			options = {
+				theme = theme,
 				icons_enabled = false,
-				-- theme = "nightfly", -- gruvbox, dracula, nightfly
-				component_separators = { left = "", right = " " },
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 			},
-			sections = {
-				-- lualine_a = { "mode" },
+			sections = process_sections({
 				lualine_a = {
 					{
 						"mode",
 					},
 				},
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"buffers",
-						mode = 0,
-						max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-						use_mode_colors = false,
-						buffers_color = {
-							active = "lualine_a_normal", -- Color for active buffer.
-							inactive = "lualine_a_inactive", -- Color for inactive buffer.
-						},
-					},
+				lualine_b = {
+					"branch",
 				},
-				lualine_x = {
+				lualine_c = {
 
+					"diff",
 					{
 						"diagnostics",
-						color = {
-							fg = colors.dracula.fg,
-							bg = colors.dracula.selection,
-						},
+						source = { "nvim" },
+						sections = { "error" },
+						diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
+					},
+					{
+						"diagnostics",
+						source = { "nvim" },
+						sections = { "warn" },
+						diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
+					},
+					{ "filename", file_status = false, path = 0 },
+					{ modified, color = { bg = colors.red } },
+					{
+						"%w",
+						cond = function()
+							return vim.wo.previewwindow
+						end,
+					},
+					{
+						"%r",
+						cond = function()
+							return vim.bo.readonly
+						end,
+					},
+					{
+						"%q",
+						cond = function()
+							return vim.bo.buftype == "quickfix"
+						end,
 					},
 					-- {
-					-- 	"diff",
-					-- 	color = {
-					-- 		fg = colors.dracula.fg,
-					-- 		bg = colors.dracula.selection,
+					-- 	"buffers",
+					-- 	mode = 0,
+					-- 	max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+					-- 	use_mode_colors = false,
+					-- 	buffers_color = {
+					-- 		active = "lualine_a_normal", -- Color for active buffer.
+					-- 		inactive = "lualine_a_inactive", -- Color for inactive buffer.
 					-- 	},
 					-- },
-					{
-						"encoding",
-						color = {
-							fg = colors.dracula.fg,
-							bg = colors.dracula.bg,
-						},
-					},
 				},
+				lualine_x = {},
 				lualine_y = {
-					{
-						"filetype",
-						color = {
-							fg = colors.dracula.fg,
-							bg = colors.dracula.selection,
-						},
-					},
+					-- "windows",
 				},
 				lualine_z = {
+
 					{
-						"progress",
-						color = {
-							fg = colors.dracula.fg,
-							bg = colors.dracula.bg,
-						},
+						"filetype",
+						-- color = {
+						-- 	fg = colors.dracula.fg,
+						-- 	bg = colors.dracula.bg,
+						-- },
 					},
+					{ "encoding" },
+					{ "location" },
+					{ "progress" },
 				},
-			},
+			}),
 		})
 	end,
 }
